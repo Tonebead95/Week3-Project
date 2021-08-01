ac842cb3bb5919a6f3ff2121f219bd2473870d92
Tonebead95 <anthonydavies160@gmail.com>
2021-08-01 13:21:02 +0100

    Learning Style Questionnaire
    Base layout of the page, Needs questions adding, css and js need adding to appropriate standard.

---
+++ quizCode.js
@@ -0,0 +1,11 @@
+/**
+ * Reuse code to show n hide questions
+ * have startQuiz as main funaction
+ * queation 1 - 5 as extention function
+ * result as last section
+ */
+
+
+/**
+ * write calculation of results here
+ */
