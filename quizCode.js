a8350c3cb0f1c84983753f185d830a3111fd5f7c
Tonebead95 <anthonydavies160@gmail.com>
2021-08-01 14:38:32 +0100

    Needs some work, ask for help you so close

--- quizCode.js
+++ quizCode.js
@@ -1,9 +1,25 @@
 /**
  * Reuse code to show n hide questions
- * have startQuiz as main funaction
+ * have nextQuestion as main funaction
  * queation 1 - 5 as extention function
  * result as last section
  */
+function nextQuestion(id1,id2) {
+    hideElement(id1);
+    showElement(id2);
+}
+function hideElement() {
+    let id = eleId.id;
+    document.getElementById(id).style.display = "none";
+}
+
+function showElement() {
+    let id = eleId.id;
+    document.getElementById(id).style.display = "inline-block";
+}
+
+
+
 
 
 /**
