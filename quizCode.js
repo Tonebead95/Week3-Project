afd796a62df317581e038ab89241bbdcb94208c6
Tonebead95 <anthonydavies160@gmail.com>
2021-08-04 18:10:41 +0100

    merging

--- quizCode.js
+++ quizCode.js
@@ -1,54 +1,23 @@
-/**
+/** 
  * Reuse code to show n hide questions
  * have nextQuestion as main funaction
  * queation 1 - 5 as extention function
  * result as last section
+*/
 
-function nextQuestion(id1,id2) {
+
+function nextQuestion(id1, id2) {
     hideElement(id1);
     showElement(id2);
 }
-function hideElement() {
-    let id = eleId.id;
+function hideElement(id) {
     document.getElementById(id).style.display = "none";
 }
 
-function showElement() {
-    let id = eleId.id;
+function showElement(id) {
     document.getElementById(id).style.display = "inline-block";
 }
- */
-
-function Start() {
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("que1").style.display = "inline-block";
-}
-function quest1() {
-    document.getElementById("que1").style.display = "none";
-    document.getElementById("que2").style.display = "inline-block";
-}
-function quest2() {
-    document.getElementById("que2").style.display = "none";
-    document.getElementById("que3").style.display = "inline-block";
-}
-function quest3() {
-    document.getElementById("que3").style.display = "none";
-    document.getElementById("que4").style.display = "inline-block";
-}
-function quest4() {
-    document.getElementById("que4").style.display = "none";
-    document.getElementById("que5").style.display = "inline-block";
-}
-
-function quest5() {
-    learningStyle()
-    document.getElementById("que5").style.display = "none";
-    document.getElementById("results").style.display = "inline-block";
-}
-function back() {
-    document.getElementById("results").style.display = "none";
-    document.getElementById("intro").style.display = "inline-block";
-}
+ 
 
 function learningStyle() {
     const radios1 = document.querySelectorAll('input[name="ques1"]');
@@ -104,7 +73,7 @@ function learningStyle() {
         msg = "You answered mostly a, you may have a visual learning style";
       }
 
-    document.getElementById("results").innerHTML = msg
+    document.getElementById("result").innerHTML = msg
 };
 
 
